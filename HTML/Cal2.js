03871dfcca349718201bace5985908b9c0967dcd
quanyb98 <quanbeo981@gmail.com>
2021-07-11 22:23:48 +0700

    bai tap dieu kien loai 2

---
+++ HTML/Cal2.js
@@ -0,0 +1,17 @@
+
+    var a = parseInt(document.getElementById("n1").value) ;
+    var b = parseInt(document.getElementById("n2").value) ;
+
+function cong(){
+    var result = a+b;
+    alert('ket qua phep cong la: ' + result);
+}function tru(){
+    var result = a-b;
+    alert('ket qua phep tru la: ' + result);
+}function nhan(){
+    var result = a*b;
+    alert('ket qua phep nhan la: ' + result);
+}function chia(){
+    var result = a/b;
+    alert('ket qua phep chia la: ' + result);
+}
---
+++ HTML/Calculator.js
@@ -0,0 +1,21 @@
+function cong(){
+    var a = parseInt(document.getElementById("n1").value) ;
+    var b = parseInt(document.getElementById("n2").value) ;
+    var result = a+b;
+    alert('ket qua phep cong la: ' + result);
+}function tru(){
+    var a = parseInt(document.getElementById("n1").value) ;
+    var b = parseInt(document.getElementById("n2").value) ;
+    var result = a-b;
+    alert('ket qua phep cong la: ' + result);
+}function nhan(){
+    var a = parseInt(document.getElementById("n1").value) ;
+    var b = parseInt(document.getElementById("n2").value) ;
+    var result = a*b;
+    alert('ket qua phep cong la: ' + result);
+}function chia(){
+    var a = parseInt(document.getElementById("n1").value) ;
+    var b = parseInt(document.getElementById("n2").value) ;
+    var result = a/b;
+    alert('ket qua phep cong la: ' + result);
+}
